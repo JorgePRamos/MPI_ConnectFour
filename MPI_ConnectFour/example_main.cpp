e94952ec828934532e98837996d91b62d1cdaae7
Jorge Pérez Ramos <jorgeperezramos99@gmail.com>
2020-05-24 14:25:18 +0200

    Initial interaction with player

---
+++ MPI_ConnectFour/example_main.cpp
@@ -0,0 +1,127 @@
+
+#include<iostream>
+#include<ctime>
+using namespace std;
+#include"board.h"	
+
+const int DEPTH = 6;	//Default depth
+
+double Evaluate(Board Current, dat LastMover, int iLastCol, int iDepth);
+/*.
+int main(int argc, char** argv)
+{
+    Board B;
+    double dResult, dBest;
+    int iBestCol, iDepth = DEPTH;
+    if (argc < 2)//No file
+    {
+        cout << "Usage: <program> <current state file> [<deep>]" << endl;
+        return 0;
+    }
+    B.Load("C:\\Users\\Jorge\\Downloads\\ploca.txt");//File Load
+    if (argc > 2)
+        iDepth = atoi(argv[2]);//Geting number of threads
+    srand((unsigned)time(NULL));
+    // check if the game is over (eg if the player has won)
+    for (int iCol = 0; iCol < B.Columns(); iCol++)
+        if (B.GameEnd(iCol))
+        {
+            cout << "Igra zavrsena!" << endl;
+            return 0;
+        }
+    // assumption: the CPU is on the move
+    do
+    {
+        cout << "Dubina: " << iDepth << endl;
+        dBest = -1; iBestCol = -1;
+        for (int iCol = 0; iCol < B.Columns(); iCol++)
+        {
+            if (B.MoveLegal(iCol))
+            {
+                if (iBestCol == -1)
+                    iBestCol = iCol;
+                B.Move(iCol, CPU);
+                dResult = Evaluate(B, CPU, iCol, iDepth - 1);
+                B.UndoMove(iCol);
+                if (dResult > dBest || (dResult == dBest && rand() % 2 == 0))
+                {
+                    dBest = dResult;
+                    iBestCol = iCol;
+                }
+                cout << "Stupac " << iCol << ", vrijednost: " << dResult << endl;
+            }
+        }
+        iDepth /= 2;
+        // why loop? if all moves lead to defeat, we count once again for twice the depth
+        // because the player may not be aware of the possibility of winning
+    } while (dBest == -1 && iDepth > 0);
+    cout << "Najbolji: " << iBestCol << ", vrijednost: " << dBest << endl;
+    B.Move(iBestCol, CPU);
+    // did we win
+
+    for (int iCol = 0; iCol < B.Columns(); iCol++)
+        if (B.GameEnd(iCol))
+        {
+            cout << "Igra zavrsena! (pobjeda racunala)" << endl;
+            return 0;
+        }
+    return 0;
+}
+*/
+
+// recursive function: examines all possible moves and returns an estimate of the obtained state of the board
+// Current: current state of the board
+// LastMover: HUMAN or CPU
+// iLastCol: previous move column
+// iDepth: depth is reduced to 0
+
+double Evaluate(Board Current, dat LastMover, int iLastCol, int iDepth)
+{
+    double dResult, dTotal;
+    dat NewMover;
+    bool bAllLose = true, bAllWin = true;
+    int iMoves;
+
+    if (Current.GameEnd(iLastCol))	// igra gotova?
+        if (LastMover == CPU)
+            return 1;	// pobjeda
+        else //if(LastMover == HUMAN)
+            return -1;	// poraz
+    // nije gotovo, idemo u sljedecu razinu
+    if (iDepth == 0)
+        return 0;	// a mozda i ne... :)
+    iDepth--;
+    if (LastMover == CPU)	// tko je na potezu
+        NewMover = HUMAN;
+    else
+        NewMover = CPU;
+    dTotal = 0;
+    iMoves = 0;	// broj mogucih poteza u ovoj razini
+    for (int iCol = 0; iCol < Current.Columns(); iCol++)
+    {
+        if (Current.MoveLegal(iCol))	// jel moze u stupac iCol
+        {
+            iMoves++;
+            Current.Move(iCol, NewMover);
+            
+            dResult = Evaluate(Current, NewMover, iCol, iDepth);
+            Current.UndoMove(iCol);
+            if (dResult > -1)
+                bAllLose = false;
+            if (dResult != 1)
+                bAllWin = false;
+            if (dResult == 1 && NewMover == CPU)
+                return 1;	// ako svojim potezom mogu doci do pobjede (pravilo 1)
+            if (dResult == -1 && NewMover == HUMAN)
+                return -1;	// ako protivnik moze potezom doci do pobjede (pravilo 2)
+            //if(dResult > 0)	// izgleda da je bolje ako se u racunanju uzimaju i porazi ...
+            dTotal += dResult;
+        }
+    }
+    if (bAllWin == true)	// ispitivanje za pravilo 3.
+        return 1;
+    if (bAllLose == true)
+        return -1;
+    dTotal /= iMoves;	// divide the rating by the number of possible moves from the given state
+    return dTotal;
+}
--- MPI_ConnectFour/main.cpp
+++ MPI_ConnectFour/main.cpp
@@ -2,125 +2,118 @@
 #include<iostream>
 #include<ctime>
 using namespace std;
-#include"board.h"	
+#include"board.h"
+const int DEPTH = 6;
+const int ROWS = 6;
+const int COLS = 7;
 
-const int DEPTH = 6;	//Default depth
 
-double Evaluate(Board Current, dat LastMover, int iLastCol, int iDepth);
+
+
+
+
+//CONNECT FOUR PROJECT
+#include<stdio.h>
+#include<string.h>
+#include<string.h>
+#include<math.h>
+#include<windows.h>
+#include<conio.h>
+char check[4];
+char yes[] = "yes";
+
+int i = 0, j = 0, k, n, row, col, value, score_x = 0, score_o = 0, game_mode = 0, number, Highscore, connecter = 0, b = 0, select_sides = 0;
+char board[100][100];
+char read[10];
+char name1[20];
+char name2[20];
+char save[] = "save";
+char sides[] = "replace";
+char red[] = "redo";
+char undo[] = "undo";
+char mains[] = "start";
+int position[100];
+int redo[100];
+
+
+//FUNCTION TO PRINT THE BOARD
+void printboard(int start, int state[ROWS][COLS]) {
+
+    int a, c;
+    //Game just started Start = 1
+    if (start) {
+        //Init with default symbol "*"                                             
+        for (a = 0; a < ROWS; a++) {
+            for (c = 0; c < COLS; c++)
+                board[a][c] = '*';
+        }
+        
+        system("cls");                          
+        system("COLOR 1A");             
+        //Board to Screen
+        for (row = 0; row < ROWS; row++) {        
+            for (col = 0; col < COLS; col++) {
+
+                printf("| %c ", board[row][col]);
+            }
+            printf("|\n");
+            for (a = 0; a < COLS; a++) printf("____");
+            printf("\n");
+        }
+        for (a = 0; a < COLS; a++)printf("%*d", 4, a + 1);
+        printf("\n");
+    }
+    else {
+
+        system("cls");                          
+        system("COLOR 1A");    
+        //Board to Screen
+        for (row = 0; row < ROWS; row++) {        
+            for (col = 0; col < COLS; col++) {
+
+                printf("| %c ", board[row][col]);
+            }
+            printf("|\n");
+            for (a = 0; a < COLS; a++) printf("____");
+            printf("\n");
+        }
+        for (a = 0; a < COLS; a++)printf("%*d", 4, a + 1);
+        printf("\n");
+
+
+    }
+
+}
 
 int main(int argc, char** argv)
 {
-    Board B;
-    double dResult, dBest;
-    int iBestCol, iDepth = DEPTH;
-    if (argc < 2)//No file
-    {
-        cout << "Usage: <program> <current state file> [<deep>]" << endl;
+    printf("####################################################################\n                              CONNECT FOUR        \n####################################################################\n");
+    printf("\n\t-Greetings MR.Player!\n\t-Press <Y> to start.\n\t-Press <N> to close.\n\t->");
+    char userInput = ' ';
+    boolean incorrect = false;
+    do{
+        if (incorrect) {
+            system("cls");
+            printf("####################################################################\n                              CONNECT FOUR        \n####################################################################\n");
+            printf("\n\t-Sorry MR.Player, I can't understand you.\n\t-Can you repeat?\n\t->");
+        }
+        incorrect = true;
+        userInput = getchar();
+        
+    
+    } while (userInput != 'Y' && userInput != 'y' && userInput != 'N' && userInput != 'n');
+
+    if (userInput == 'n' || userInput == 'N') {
+        system("cls");
+        printf("####################################################################\n                              CONNECT FOUR        \n####################################################################\n");
+        printf("\n\t-Goodbye! Have nice day MR.Player!\n\n\n");
+
+
         return 0;
     }
-    B.Load("C:\\Users\\Jorge\\Downloads\\ploca.txt");//File Load
-    if (argc > 2)
-        iDepth = atoi(argv[2]);//Geting number of threads
-    srand((unsigned)time(NULL));
-    // check if the game is over (eg if the player has won)
-    for (int iCol = 0; iCol < B.Columns(); iCol++)
-        if (B.GameEnd(iCol))
-        {
-            cout << "Igra zavrsena!" << endl;
-            return 0;
-        }
-    // assumption: the CPU is on the move
-    do
-    {
-        cout << "Dubina: " << iDepth << endl;
-        dBest = -1; iBestCol = -1;
-        for (int iCol = 0; iCol < B.Columns(); iCol++)
-        {
-            if (B.MoveLegal(iCol))
-            {
-                if (iBestCol == -1)
-                    iBestCol = iCol;
-                B.Move(iCol, CPU);
-                dResult = Evaluate(B, CPU, iCol, iDepth - 1);
-                B.UndoMove(iCol);
-                if (dResult > dBest || (dResult == dBest && rand() % 2 == 0))
-                {
-                    dBest = dResult;
-                    iBestCol = iCol;
-                }
-                cout << "Stupac " << iCol << ", vrijednost: " << dResult << endl;
-            }
-        }
-        iDepth /= 2;
-        // why loop? if all moves lead to defeat, we count once again for twice the depth
-        // because the player may not be aware of the possibility of winning
-    } while (dBest == -1 && iDepth > 0);
-    cout << "Najbolji: " << iBestCol << ", vrijednost: " << dBest << endl;
-    B.Move(iBestCol, CPU);
-    // did we win
 
-    for (int iCol = 0; iCol < B.Columns(); iCol++)
-        if (B.GameEnd(iCol))
-        {
-            cout << "Igra zavrsena! (pobjeda racunala)" << endl;
-            return 0;
-        }
-    return 0;
 }
 
 
-// recursive function: examines all possible moves and returns an estimate of the obtained state of the board
-// Current: current state of the board
-// LastMover: HUMAN or CPU
-// iLastCol: previous move column
-// iDepth: depth is reduced to 0
 
-double Evaluate(Board Current, dat LastMover, int iLastCol, int iDepth)
-{
-    double dResult, dTotal;
-    dat NewMover;
-    bool bAllLose = true, bAllWin = true;
-    int iMoves;
 
-    if (Current.GameEnd(iLastCol))	// igra gotova?
-        if (LastMover == CPU)
-            return 1;	// pobjeda
-        else //if(LastMover == HUMAN)
-            return -1;	// poraz
-    // nije gotovo, idemo u sljedecu razinu
-    if (iDepth == 0)
-        return 0;	// a mozda i ne... :)
-    iDepth--;
-    if (LastMover == CPU)	// tko je na potezu
-        NewMover = HUMAN;
-    else
-        NewMover = CPU;
-    dTotal = 0;
-    iMoves = 0;	// broj mogucih poteza u ovoj razini
-    for (int iCol = 0; iCol < Current.Columns(); iCol++)
-    {
-        if (Current.MoveLegal(iCol))	// jel moze u stupac iCol
-        {
-            iMoves++;
-            Current.Move(iCol, NewMover);
-            dResult = Evaluate(Current, NewMover, iCol, iDepth);
-            Current.UndoMove(iCol);
-            if (dResult > -1)
-                bAllLose = false;
-            if (dResult != 1)
-                bAllWin = false;
-            if (dResult == 1 && NewMover == CPU)
-                return 1;	// ako svojim potezom mogu doci do pobjede (pravilo 1)
-            if (dResult == -1 && NewMover == HUMAN)
-                return -1;	// ako protivnik moze potezom doci do pobjede (pravilo 2)
-            //if(dResult > 0)	// izgleda da je bolje ako se u racunanju uzimaju i porazi ...
-            dTotal += dResult;
-        }
-    }
-    if (bAllWin == true)	// ispitivanje za pravilo 3.
-        return 1;
-    if (bAllLose == true)
-        return -1;
-    dTotal /= iMoves;	// divide the rating by the number of possible moves from the given state
-    return dTotal;
-}
